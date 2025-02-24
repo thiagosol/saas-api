677b1ac89d70b10fa14885ced22a31adc150d61d
Thiago Sol <thiagosol@ymail.com>
2025-02-23 23:32:36 -0300

    refac: delete old tests

--- src/test/java/com/thiagosol/GreetingResourceIT.java
+++ src/test/java/com/thiagosol/GreetingResourceIT.java
@@ -2,7 +2,7 @@ package com.thiagosol;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@QuarkusIntegrationTest
+//@QuarkusIntegrationTest
 class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
